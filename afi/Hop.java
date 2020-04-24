4536d104bb267bd2c57e4c37d65051d5ecb2b1a1
ryanriopelle <rriopell@eng.ucsd.edu>
2020-04-23 20:59:41 -0700

    most recent update

--- afi/Hop.java
+++ afi/Hop.java
@@ -1,48 +1,56 @@
 /**
- * File Name: Hop.java 
+ * File Name: Hop.java
  * Hop concrete class
- * 
- * 
+ *
+ *
  * To Compile: IntUtil.java RandomInt.java Hop.java HopBase.java
- * 
- * @author Jagadeesh Vasudevamurthy
- * @year 2019
- */
-//
-//class Hop extends HopBase{
-//	//You cannot have any functions or data here
-//	Hop() {
-//		//NOTHING CAN BE CHANGED HERE
-//		testBed();
-//	}
-//
-//	@Override
-//  int hopSmart(int [] s, int x) {
-//		//NOTHING CAN BE CHANGED HERE
-//    return alg(s,x) ;
-//	}
-//
-//	/*
-//	 * WRITE CODE IN alg
-//	 *  YOU CANNOT USE ANY static variable in this function
-//	 *  YOU can use many local variables inside the function
-//	 *  Cannot use any loop statements like:  for, while, do, while, go to
-//	 *  Can use if
-//	 *  ONLY AFTER THE execution of this routine array s MUST have the same contents as you got it.
-//	 *  YOU cannot call any subroutine inside this function except alg itself
-//	 *
-//	 */
-//
-//	private int alg(int [] s, int x) {
-//		//WRITE CODE
-//		//If your code is more than 10 lines, you don't understand the problem
-//	}
-//
-//	public static void main(String[] args) {
-//		//NOTHING CAN BE CHANGED HERE
-//		System.out.println("Hop problem STARTS");
-//		Hop m = new Hop() ;
-//		System.out.println("All Hop tests passed. You are great");
-//		System.out.println("Hop problem ENDS");
-//	}
-//}
+ *
+ * 	/*
+ * 	 * WRITE CODE IN alg
+ * 	 *  YOU CANNOT USE ANY static variable in this function
+ * 	 *  YOU can use many local variables inside the function
+ * 	 *  Cannot use any loop statements like:  for, while, do, while, go to
+ * 	 *  Can use if
+ * 	 *  ONLY AFTER THE execution of this routine array s MUST have the same contents as you got it.
+ * 	 *  YOU cannot call any subroutine inside this function except alg itself
+ * 	 *
+ *          */
+
+
+class Hop extends HopBase{
+	//You cannot have any functions or data here
+	Hop() {
+		//NOTHING CAN BE CHANGED HERE
+		testBed();
+	}
+
+//	public static int hopNum = 0;
+
+	@Override
+  	int hopSmart(int [] s, int x) {
+		//NOTHING CAN BE CHANGED HERE
+    	return alg(s,x);
+	}
+
+	private int alg(int [] s, int x) {
+		int hopNum = 0;
+		void do_count(int [] s, int x, hopNum){
+			// Base case
+			if (s[x] == x || hopNum>=s.length){
+				return hopNum;
+			}
+			else {
+				return do_count(s,s[x], hopNum+1);
+			}
+		}
+		return hopNum;
+	}
+
+	public static void main(String[] args) {
+		//NOTHING CAN BE CHANGED HERE
+		System.out.println("Hop problem STARTS");
+		Hop m = new Hop() ;
+		System.out.println("All Hop tests passed. You are great");
+		System.out.println("Hop problem ENDS");
+	}
+}
